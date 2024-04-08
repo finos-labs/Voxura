de95b6ef5bd24e4f9cc88f4288a92b3cd379a8c8
Velvárt András <andras.velvart@aerinx.com>
2024-04-08 18:06:14 +0200

    Added microphone button to the wpf app

--- Voxura/Voxura.WpfDemo/MainWindow.xaml.cs
+++ Voxura/Voxura.WpfDemo/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Windows;
 using Voxura.WpfDemo.ViewModels;
+using WindowsInput;
 
 namespace Voxura.WpfDemo
 {
@@ -17,6 +18,18 @@ namespace Voxura.WpfDemo
             InitializeComponent();
             _viewModel.Initialize(DesignerProperties.GetIsInDesignMode(this));
             DataContext = _viewModel;
+        }
+
+        private void StartDictation()
+        {
+            UserText.Focus();
+            // send win+h to start dictation
+            new InputSimulator().Keyboard.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.VK_H);
+        }
+
+        private void DictateButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartDictation();
         }
     }
 }
